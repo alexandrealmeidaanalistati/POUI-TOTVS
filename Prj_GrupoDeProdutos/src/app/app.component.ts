37015ac4f5b356571fbee4d64d4f37e4f406d54c
Alexandre Almeida <alexandrealmeidaanalistati@gmail.com>
2025-05-09 15:47:59 -0300

    Configurando pacotes Angular

--- Prj_GrupoDeProdutos/src/app/app.component.ts
+++ Prj_GrupoDeProdutos/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { RouterOutlet, Router } from '@angular/router';
 
 import {
   PoMenuItem,
@@ -10,21 +12,29 @@ import {
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [
     CommonModule,
+    RouterOutlet,
     PoToolbarModule,
     PoMenuModule,
-    PoPageModule
+    PoPageModule,
+    HttpClientModule
   ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
+
 export class AppComponent {
+
   readonly menus: Array<PoMenuItem> = [
-    { label: 'Home', action: this.onClick.bind(this) },
+    { label: 'Cadastro do Grupo', action: this.onClick.bind(this), icon: 'po-icon-clipboard', shortLabel: 'Cadastro' },
+    { label: 'Ajuda (Help)',      action: this.onClick.bind(this), icon: 'po-icon-help',      shortLabel: 'Ajuda' },
+    { label: 'Sair',              action: this.onClick.bind(this), icon: 'po-icon-exit',      shortLabel: 'Sair' }
   ];
 
   private onClick() {
-    alert('Clicked in menu item');
+    alert("Clicked in menu item");
   }
 }
+
